005a8e3f1436a50cf48391ac026cb7e9172c26ae
conolo <conolo@qq.com>
2024-11-18 10:47:34 +0800

    Tercent

--- action.sh
+++ action.sh
@@ -2,6 +2,6 @@
 #sh /usr/share/wifi/get.sh
 /etc/init.d/alist stop
 chmod +x alist
-cp /mnt/sda/iptv/alist /mnt/sda/iptv/alist
+cp /mnt/sda/iptv/alist /mnt/sda/alist/alist
 /etc/init.d/alist start
 rm -rf alist
