33a77b8995a2ce8399f5182869b96878ebaee76f
conolo <conolo@qq.com>
2024-11-22 15:48:52 +0800

    update

--- action.sh
+++ action.sh
@@ -1,13 +1,2 @@
 #for activate my rule
 #sh /usr/share/wifi/get.sh
-/etc/init.d/alist stop
-cd /mnt/sda/gitee/
-git pull
-chmod +x alist
-mv alist /mnt/sda/alist/
-git add -A
-git commit -m 'update'
-git push origin main --force
-/etc/init.d/homeproxy stop
-/etc/init.d/alist start
-cd /mnt/sda/iptv/
