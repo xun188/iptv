d79fc7523ccb819874fd2363356f7caad4422f50
conolo <conolo@qq.com>
2024-11-25 14:49:12 +0800

    update

--- action.sh
+++ action.sh
@@ -1,7 +1,2 @@
 #for activate my rule
-cp /mnt/sda/iptv/homeproxy /etc/config/homeproxy
-/etc/init.d/homeproxy restart
-sleep 5
-rm -rf /mnt/sda/iptv/homeproxy
-rm -rf /mnt/sda/gitee/homeproxy
-sh /usr/share/wifi/get.sh
+#sh /usr/share/wifi/get.sh
